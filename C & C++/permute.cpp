ede2bfba6961566c976cc1cd370cd3a75002573a
Atharva <atharvah53@gmail.com>
2025-11-21 22:14:57 -0700

    permutation

---
+++ C & C++/permute.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+void permutation(string a , int i , int n)
+{
+    if (i == n)
+    {
+        cout << a << endl;
+    }
+    else
+    {
+        for(int j = i; j < n; j++)
+        {
+            swap(a[i] , a[j]);
+            permutation(a , i + 1, n);
+            swap(a[i] , a[j]);
+        }
+    }
+}
+
+int main()
+{
+    string a = "abc";
+    permutation(a , 0 , 3);
+
+}
