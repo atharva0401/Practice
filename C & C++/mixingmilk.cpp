dd3aedb9a296bba22f4ce32f399fd20452f2a2b9
Atharva <atharvah53@gmail.com>
2025-11-24 23:07:25 -0700

    Modified problem

--- C & C++/mixingmilk.cpp
+++ C & C++/mixingmilk.cpp
@@ -1,58 +1,46 @@
-#include <iostream>
 #include <algorithm>
 #include <cstdio>
+#include <iostream>
+#include <vector>
 
-int main()
-{
-    freopen("mixmilk.in" , "r" , stdin);
-    int milk[3] = {0};
-    int a , b , c;
-    scanf("%d %d" , &a , &milk[0]);
-    scanf("%d %d" , &b , &milk[1]);
-    scanf("%d %d" , &c , &milk[2]);
-    
-    for(int i = 0; i < 100 ; i++)
-    {
-        if(i % 3 == 0)
-        {
-            if(milk[0] + milk[1] < b)
-            {
-                milk[1] = milk[1] + milk[0];
-                milk[0] = 0;
-            }
-            else
-            {
-                milk[0] += milk[1] - b;
-                milk[1] = b;
-            }
-        }
-        else if(i % 3 == 1)
-        {
-            if(milk[1] + milk[2] < c)
-            {
-                milk[2] = milk[1] + milk[2];
-                milk[1] = 0;
-            }
-            else
-            {
-                milk[1] += milk[2] - c;
-                milk[2] = c;
-            }
-        }
-        else
-        {
-            if(milk[0] + milk[2] < a)
-            {
-                milk[0] = milk[0] + milk[2];
-                milk[2] = 0;
-            }
-            else
-            {
-                milk[2] += milk[0] - a;
-                milk[0] = a;
-            }
-        }
-    }
-    freopen("mixmilk.out" , "w" , stdout);
-    printf("%d\n%d\n%d" , milk[0], milk[1], milk[2]);
+
+using namespace std;
+
+
+const int N = 3;
+
+const int TURN_NUM = 100;
+
+
+int main() {
+
+	freopen("mixmilk.in", "r", stdin);
+
+	vector<int> capacity(N);
+
+	vector<int> milk(N);
+
+	for (int i = 0; i < N; i++) { scanf("%d %d", &capacity[i], &milk[i]); }
+
+
+	for (int i = 0; i < TURN_NUM; i++) {
+
+		int bucket1 = i % N;
+
+		int bucket2 = (i + 1) % N;
+
+		int amt = min(milk[bucket1], capacity[bucket2] - milk[bucket2]);
+
+
+		milk[bucket1] -= amt;
+
+		milk[bucket2] += amt;
+
+	}
+
+
+	freopen("mixmilk.out", "w", stdout);
+
+	for (int m : milk) { cout << m << '\n'; }
+
 }
