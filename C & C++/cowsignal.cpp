886b5e7ccb0a173ed046aa999b9d505015ce520e
Atharva <atharvah53@gmail.com>
2025-11-26 22:48:50 -0700

    cowsignal update

---
+++ C & C++/cowsignal.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <cstdio>
+
+using namespace std;
+
+int main()
+{
+    freopen("cowsignal.in" , "r" , stdin);
+    freopen("cowsignal.out" , "w" , stdout);
+    int m , n , k;
+    string s;
+    cin >> m >> n >> k;
+    while(getline(cin ,s ))
+    {
+        for(int j = 0; j < k; j++)
+        {
+            char a[n*k];
+            for(int i : s)
+            {
+                for(int l = 0 ; l < k; l++)
+                {
+                    
+                }
+            }
+        }
+
+    }
+}
