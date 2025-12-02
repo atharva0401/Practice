9af80a549d035a68d5cfd3dee6848a0fd38606ef
Atharva <atharvah53@gmail.com>
2025-12-01 23:45:04 -0700

    Final lostcow

--- C & C++/lostcow.cpp
+++ C & C++/lostcow.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <cstdio>
-
+#include <cmath>
 using namespace std;
 
 int main()
@@ -9,6 +9,27 @@ int main()
     freopen("lostcow.in" , "r" , stdin);
     freopen("lostcow.out" , "w" , stdout);
     scanf("%d %d" , &x , &y);
-    
+    int temp = 0;
+    int i = 0;
+    int distance = 0;
+    int step = 1;
+    int curr = x;
+    while(true)
+    {
+        temp = x + pow(-1 , i)*step;
+        if((x <= y && y <= temp) || (temp <= y && y <= x))
+        {
+            distance = distance + abs(curr - y);
+            break;
+        }
+        else
+        {
+            distance = distance + abs(temp - curr);
+            curr = temp;
+            i = i + 1;
+            step = step * 2;
+        } 
+    }
+    cout << distance;
     
 }
