97334500d884ce77d49b05f174d86869ecbe1729
Atharva <atharvah53@gmail.com>
2025-12-05 09:29:06 -0700

    Final Bovine Shuffle

--- C & C++/bovineshuffle.cpp
+++ C & C++/bovineshuffle.cpp
@@ -7,22 +7,26 @@ int main()
     freopen("shuffle.in" , "r" , stdin);
     freopen("shuffle.out" , "w" , stdout);
     int n;
+    scanf("%d" , &n);
     int shuffle[n];
     int final[n];
-    scanf("%d" , &n);
     for(int i = 0; i < n ; i++)
     {
-        scanf("%d" , shuffle[i]);
+        scanf("%d" , &shuffle[i]);
         shuffle[i]--;
     }
     for(int i = 0 ; i < n ; i ++)
     {
-        scanf("%d" , final[i]);
+        scanf("%d" , &final[i]);
     }
 
     for(int i = 0 ; i < n; i++)
     {
-        int c = final[shuffle[shuffle[i]]];
-        printf("%d" , &c);
+        int c = final[shuffle[shuffle[shuffle[i]]]];
+        printf("%d" , c);
+        if(i != n - 1)
+        {
+            printf("\n");
+        }
     }
 }
