9a5c83b33061fa6a8d54e90b79bf3f1dcb7791e7
Atharva <atharvah53@gmail.com>
2025-12-07 22:43:54 -0700

    Initial traffic

---
+++ C & C++/measuringtraffic.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    freopen("traffic.in" , "r" , stdin);
+    freopen("traffic.out" , "w" , stdout);
+    int n;
+    scanf("%d" , &n);
+    
+}
